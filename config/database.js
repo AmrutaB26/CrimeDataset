685b60d6614d56feff594d790381e0979a8bb023
AasthaJhunjhanuwala <ajhunjhanuwala@ufl.edu>
2019-04-25 15:46:06 -0400

    DB config mod

--- config/database.js
+++ config/database.js
@@ -2,9 +2,6 @@ module.exports = {
     hrPool: {
       user: "aj3",
       password: "Database1",
-      connectString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle.cise.ufl.edu)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=orcl)))",
-      poolMin: 10,
-      poolMax: 10,
-      poolIncrement: 0
+      connectString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle.cise.ufl.edu)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=orcl)))"
     }
   };
