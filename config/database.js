7bce6d779ac244bfaa78b97fa07698bb54405645
Amruta <amruta.basrur@ufl.edu>
2019-04-26 15:19:34 -0400

    changing area related queries

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ module.exports = {
       user: "aj3",
       password: "Database1",
       connectString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle.cise.ufl.edu)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=orcl)))",
-      poolMin: 10,
-      poolMax: 10,
-      poolIncrement: 0
+      // poolMin: 10,
+      // poolMax: 10,
+      // poolIncrement: 0
     }
   };
